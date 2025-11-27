b1c8202a0e3dffd5bd97f6a14ed60c262af50416
Jtech99 <eugi202205@gmail.com>
2025-11-27 22:36:49 +0000

    commit some comments for the functions

--- _printf.c
+++ _printf.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-// recursive function
-int print_number(unsigned int num) 
+
+/* recursive function to print an unsigned number */
+int print_number(unsigned int num)
 {
-	int count = 0;
-	// if num / 10 != 0 stillnumber to display
-	if (num / 10)
-		count += print_number(num / 10);
-	_putchar((num % 10) + '0');
-	count++;
-	return (count);
+    int count = 0;
+
+    /* if num / 10 != 0, there are more digits to display */
+    if (num / 10)
+        count += print_number(num / 10);
+
+    _putchar((num % 10) + '0');
+    count++;
+    return (count);
 }
 
 /**
@@ -21,79 +24,86 @@ int print_number(unsigned int num)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	int i = 0;
+    va_list args;
+    int count = 0;
+    int i = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else if (format[i] == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				char *s = va_arg(args, char *);
-				if (s == NULL)
-					s = "(null)";
-				while (*s)
-				{
-					_putchar(*s);
-					count++;
-					s++;
-				}
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				int num = va_arg(args, int);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                _putchar(c);
+                count++;
+            }
+            else if (format[i] == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (s == NULL)
+                    s = "(null)";
+                while (*s)
+                {
+                    _putchar(*s);
+                    count++;
+                    s++;
+                }
+            }
+            else if (format[i] == 'd' || format[i] == 'i')
+            {
+                int num = va_arg(args, int);
 
-				if (num < 0)
-				{
-					_putchar('-');
-					count++;
-					num = -num;
-				}
+                if (num < 0)
+                {
+                    _putchar('-');
+                    count++;
+                    /* handle INT_MIN safely */
+                    if (num == -2147483648)
+                    {
+                        count += print_number(2147483648U);
+                        i++;
+                        continue;
+                    }
+                    num = -num;
+                }
 
-				if (num == 0)
-				{
-					_putchar('0');
-					count++;
-				}
-				else
-				{
-					count += print_number((unsigned int)num);
-				}
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
-	}
+                if (num == 0)
+                {
+                    _putchar('0');
+                    count++;
+                }
+                else
+                {
+                    count += print_number((unsigned int)num);
+                }
+            }
+            else
+            {
+                _putchar('%');
+                _putchar(format[i]);
+                count += 2;
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            count++;
+        }
+        i++;
+    }
 
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
