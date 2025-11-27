8cb390b52da13ef7ed1c9828a5336df4c73acdc9
Jtech99 <eugi202205@gmail.com>
2025-11-27 23:19:44 +0000

    Accepted incoming changes in main.c

--- _printf.c
+++ _printf.c
@@ -39,6 +39,7 @@ int _printf(const char *format, ...)
         {
             i++;
             if (format[i] == '\0')
+                count--;
                 break;
 
             if (format[i] == '%')
